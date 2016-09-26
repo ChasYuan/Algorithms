f207fecce4f228f1365953ce33897077204e38d5
ChasYuan <chasyuan@gmail.com>
2016-09-25 19:05:39 -0700

    comment

---
+++ src/chas/sort/MaxPQ.java
@@ -0,0 +1,87 @@
+package chas.sort;
+
+public class MaxPQ<Key extends Comparable<Key>>{
+	private Key[] pq;
+	private int N = 0;
+	
+	public MaxPQ(int maxN){
+		pq = (Key[]) new Comparable[maxN + 1];
+	}
+	
+	public boolean isEmpty(){
+	return N == 0;	
+	}
+	
+	public int size(){
+		return N;
+	}
+	
+	public void insert(Key v){
+		pq[++N] = v;
+		swim(N);
+	}
+	
+	public Key delMax(){
+		Key max = pq[1];
+		Key tmp = pq[N];
+		pq[N--] = pq[1];
+		pq[1] = tmp;
+		pq[N+1] = null;
+		sink(1);
+		return max;
+		
+	}
+	public void swim(int k){
+		while(k > 1 && pq[k/2].compareTo(pq[k]) < 0)
+		{
+			Key tmp = pq[k/2];
+			pq[k/2] = pq[k];
+			pq[k] = tmp;
+			k = k / 2;
+		}
+		
+		
+	}
+	
+	public void sink(int k){
+		
+		while(2*k <= N){
+			int j = k * 2;
+			if(j < N && pq[j].compareTo(pq[j+1]) < 0)
+				j += 1;
+			if(pq[j].compareTo(pq[k]) > 0){
+			Key tmp = pq[j];
+			pq[j] = pq[k];
+			pq[k] = tmp;
+			k = j;
+			}
+			else
+				break;
+		}
+	}
+	public String toString(){
+		String s = "";
+		for(int i = 1; i <= N;i++)
+			s =s + pq[i].toString() +" ";
+		return s;
+	}
+	
+//	public static void main(String[] args){
+//		MaxPQ<Integer> pq = new MaxPQ<Integer>(16);
+//		for(int i = 0 ; i < 16; i++){
+//			pq.insert((int)((Math.random() * 10)+3));
+//		}
+//		System.out.println(pq.isEmpty());
+//		System.out.println(pq.size());
+//		System.out.println(pq.toString());
+//		pq.delMax();
+//		System.out.println(pq.toString());
+//		pq.insert(13);
+//		System.out.println(pq.toString());
+//		pq.delMax();
+//		System.out.println(pq.toString());
+//		pq.insert(1);
+//		System.out.println(pq.toString());
+//		
+//	}
+}
